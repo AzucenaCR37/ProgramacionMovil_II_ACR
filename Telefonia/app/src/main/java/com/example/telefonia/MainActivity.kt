e3228b538e832f80192390358b3a93c19ccb762c
Azucena Camargo Ruiz <s20120176@alumnos.itsur.edu.mx>
2024-03-10 18:03:33 -0600

    Interfaz de la práctica: BroadcastReceiver y Telefonía: Respuesta automática

---
+++ Telefonia/app/src/main/java/com/example/telefonia/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.telefonia
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.telefonia.ui.theme.TelefoniaTheme
+import com.example.telefonia.vm.ViewModelSMS
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TelefoniaTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.error
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier,
+             viewModel: ViewModelSMS = viewModel (factory= ViewModelSMS.Factory )) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .padding(40.dp)
+            .fillMaxWidth()
+    ){
+
+        Spacer(modifier = Modifier.height(200.dp))
+        Row(
+        ){
+
+            Text(
+                text= "Automatic SMS Telephony",
+                fontSize = 21.sp,
+                fontFamily =  FontFamily.SansSerif,
+                fontWeight = FontWeight.ExtraBold,
+            )
+        }
+        Spacer(modifier = Modifier.height(25.dp))
+        OutlinedTextField(
+            value = viewModel.phone,
+            label = { Text(text="Type the phone number")},
+            onValueChange ={
+                viewModel.phoneUpdate(it)
+            },
+            modifier= Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(25.dp))
+        OutlinedTextField(
+            value = viewModel.sms,
+            label = { Text(text="Type the SMS")},
+            onValueChange ={
+                viewModel.smsUpdate(it)
+            },
+            modifier= Modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = {
+                // Acción al hacer clic en el botón
+            },
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text("Send SMS")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TelefoniaTheme {
+        Greeting()
+    }
+}
---
+++ Telefonia/app/src/main/java/com/example/telefonia/vm/ViewModelSMS.kt
@@ -0,0 +1,32 @@
+package com.example.telefonia.vm
+
+import android.provider.ContactsContract.CommonDataKinds.Phone
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+class ViewModelSMS():ViewModel(){
+    var phone by mutableStateOf("")
+
+    fun phoneUpdate(number: String){
+        phone=number
+    }
+
+    var sms by mutableStateOf("")
+
+    fun smsUpdate(msg: String){
+        sms=msg
+    }
+
+    companion object{
+        val Factory= viewModelFactory {
+            initializer {
+                ViewModelSMS()
+            }
+        }
+    }
+}
