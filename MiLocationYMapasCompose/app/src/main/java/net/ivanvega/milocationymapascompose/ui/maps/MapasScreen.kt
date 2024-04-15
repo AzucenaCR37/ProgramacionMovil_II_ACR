c8de1f9672fc7125f21b2a33bd6de8475151cd0d
Azucena Camargo Ruiz <s20120176@alumnos.itsur.edu.mx>
2024-04-14 19:09:07 -0600

    Paso: Controlling a map's camera Paso: Creating and configurin a map (este ya estaba en el archivo antes de este cambio sólo se que se paso documentarlo es la función MiMapa)

--- MiLocationYMapasCompose/app/src/main/java/net/ivanvega/milocationymapascompose/ui/maps/MapasScreen.kt
+++ MiLocationYMapasCompose/app/src/main/java/net/ivanvega/milocationymapascompose/ui/maps/MapasScreen.kt
@@ -1,10 +1,15 @@
 package net.ivanvega.milocationymapascompose.ui.maps
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
@@ -25,5 +30,21 @@ fun MiMapa(){
             title = "Singapore",
             snippet = "Marker in Singapore"
         )
+    }
+}
+
+@Composable
+fun ControllingCamera(){
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState: CameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 11f)
+    }
+    Box(Modifier.fillMaxSize()) {
+        GoogleMap(cameraPositionState = cameraPositionState)
+        Button(onClick = {
+            cameraPositionState.move(CameraUpdateFactory.zoomIn())
+        }) {
+            Text(text = "Zoom In")
+        }
     }
 }
